d1cb43df2ac2667410cec301a08c283f673fae53
Archive 15 Design <archive15design@gmail.com>
2023-10-30 15:24:26 +0100

    all tests passed

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,31 @@
 
 function bubbleSort(arr) {
+    let noSwap = true;
 
     // Iterate through the array
+    for (let i = 0; i < arr.length; i++){
+      let current = arr[i];
+      let next = arr[i+1];
 
       // If the current value is greater than its neighbor to the right
+      if (current > next) {
         // Swap those values
-
         // Do not move this console.log
+        [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
         console.log(arr.join(","));
+        noSwap = false;
+        }
+      }
 
     // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
+      if (noSwap) return;
+      // Otherwise, repeat from the beginning
+      else bubbleSort(arr);
 
 }
+
+let testArr = [2,4,6,8,1,3,5,7,9];
+
+bubbleSort(testArr);
 
 module.exports = bubbleSort;
